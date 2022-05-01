954f7ff3c00de7c2b0f9dfc74535d0251ea48513
Jonas Höchst <git@jonashoechst.de>
2022-05-01 13:21:37 +0200

    title from config

--- registration/public.py
+++ registration/public.py
@@ -1,5 +1,7 @@
 import flask
 from . import models
+from flask import current_app
+
 
 public_bp = flask.Blueprint('public', __name__, url_prefix='', static_folder='static')
 
@@ -9,4 +11,4 @@ public_bp = flask.Blueprint('public', __name__, url_prefix='', static_folder='st
 def index():
     _events = models.Event.query.all()
     _groups = models.Group.query.all()
-    return flask.render_template('index.html', title='PfadiTag 2022', events=_events, groups=_groups)
+    return flask.render_template('index.html', title=current_app.config["APP_TITLE"], events=_events, groups=_groups)
