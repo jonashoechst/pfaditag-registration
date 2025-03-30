cbda2ca04d7dc8d98f929827a5c277f9ee3fbb96
Jonas Höchst <git@jonashoechst.de>
2025-03-30 16:04:18 +0200

    update 2025

--- registration/__init__.py
+++ registration/__init__.py
@@ -30,7 +30,7 @@ mail = Mail()
 
 def create_app():
     app = Flask(__name__)
-    app.config['APP_TITLE'] = "PfadiTag 2024"
+    app.config['APP_TITLE'] = "PfadiTag 2025"
     app.config.from_prefixed_env()
 
     Bootstrap5(app)
